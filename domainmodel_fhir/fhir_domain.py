ef9c77628206640580c38f36a8ebc75b398d9568
RFWiegerinck <rob.wiegerinck@nlhealthcareclinics.com>
2016-10-17 13:35:49 +0200

    domainmodel_fhir: format aangepast

--- domainmodel_fhir/fhir_domain.py
+++ domainmodel_fhir/fhir_domain.py
@@ -10,54 +10,6 @@ from pyelt.datalayers.dv import DvEntity, Sat, HybridSat, Link, LinkReference
 #########################################################################################################
 
 
-class Patient(DvEntity, Entity):   # FHIR type: DomainResource (http://hl7.org/fhir/domainresource.html#1.20)
-
-    class Default(Sat):
-        active = Columns.BoolColumn()  # patient record active?
-        gender = Columns.RefColumn(RefTypes.geslacht_types)
-        birthdate = Columns.DateColumn()
-        # deceased + deceased_date voorbeeld van "type[x]" , meer dan 1 datatype voor content. (=polymorphic in OO)
-        # voor meer info zie http://hl7.org/fhir/formats.html#choice
-        deceased_boolean = Columns.BoolColumn()
-        deceased_datetime = Columns.DateTimeColumn()
-        # type[x]
-        multiple_birth_boolean = Columns.BoolColumn()
-        multiple_birth_integer = Columns.IntColumn()
-
-    class Identifier(HybridSat):      #FHIR type: Element (http://hl7.org/fhir/element.html#1.21.0)
-        class use(HybridSat.Types):
-            usual = 'usual'
-            official = 'official'
-            temp = 'temp'
-            secondary = 'secondary (If known)'
-        # class type(HybridSat.Types):    # type heeft FHIR type: CodeableConcept (http://hl7.org/fhir/datatypes.html#codeableconcept)
-        # # class type(Sat):  # todo: misschien op deze manier beter?
-        #     text = Columns.TextColumn() # plain text weergave van het concept
-        #     class coding(HybridSat.Types.Coding):  # coding heeft FHIR type: Element; todo: hoe om te gaan met deze extra laag? HybridSat.Types.Coding bestaat nog niet
-        #     # class coding(Sat):   # todo: misschien op deze manier beter?
-        #         system = Columns.TextColumn()   #system heeft FIHR type: uri; Uniform Resource Identifier ( http://hl7.org/fhir/datatypes.html#uri)
-        #         version = Columns.TextColumn()
-        #         code = Columns.TextColumn()  # symbool in syntax gedefinieerd door het systeem (http://hl7.org/fhir/datatypes.html#code)
-        #         display = Columns.TextColumn()  # weergave gedefinieerd door het systeem
-        #         user_selected = Columns.BoolColumn()  # indien deze codering door de user zelf was gekozen
-
-        type_text = Columns.TextColumn(CodeableConcept.text)
-        # coding_code = Columns.TextColumn(CodeableConcept.system)
-        coding_system = Columns.TextColumn(Coding.system)   #system heeft FIHR type: uri; Uniform Resource Identifier ( http://hl7.org/fhir/datatypes.html#uri)
-        coding_version = Columns.TextColumn(Coding.version)
-        coding_code = Columns.TextColumn(Coding.code)  # symbool in syntax gedefinieerd door het systeem (http://hl7.org/fhir/datatypes.html#code)
-        coding_display = Columns.TextColumn(Coding.display)  # weergave gedefinieerd door het systeem
-        coding_user_selected = Columns.BoolColumn(Coding.user_selected)  # indien deze codering door de user zelf was gekozen
-
-        period_start = Columns.DateTimeColumn(Period.start)
-        # period_start = Period.start
-        period_end = Columns.DateTimeColumn(Period.end)
-
-    class Name(Sat):
-
-
-
-"""utility classes?:"""
 
 class Coding:
     system = Columns.TextColumn()   #system heeft FIHR type: uri; Uniform Resource Identifier ( http://hl7.org/fhir/datatypes.html#uri)
@@ -87,8 +39,70 @@ class HumanName:    #FHIR type: Element
     given = Columns.TextColumn()    # given names (not always 'first'); includes middle names
     prefix = Columns.TextColumn()   # Parts that come before the name
     suffix = Columns.TextColumn()   # Part that come after the name
-    period_start = Columns.DateTimeColumn(Period.start)
-    # period_start = Period.start
-    period_end = Columns.DateTimeColumn(Period.end)
+    # period_start = Columns.DateTimeColumn(Period.start)
+    start = Period.start
+    end = Period.end
+
+# todo: mag weg na testen:
+# humanname = HumanName()
+# print(type(humanname))
+# print(type(humanname.use))
+# print(type(humanname.use.maiden))
+# print(type(Period.start))
+# print(type(humanname.start))
+
+
+class Patient(DvEntity, Entity):   # FHIR type: DomainResource (http://hl7.org/fhir/domainresource.html#1.20)
+
+    class Default(Sat):
+        active = Columns.BoolColumn()  # patient record active?
+        gender = Columns.RefColumn(RefTypes.geslacht_types)
+        birthdate = Columns.DateColumn()
+        # deceased + deceased_date voorbeeld van "type[x]" , meer dan 1 datatype voor content. (=polymorphic in OO)
+        # voor meer info zie http://hl7.org/fhir/formats.html#choice
+        deceased_boolean = Columns.BoolColumn()
+        deceased_datetime = Columns.DateTimeColumn()
+        # type[x]
+        multiple_birth_boolean = Columns.BoolColumn()
+        multiple_birth_integer = Columns.IntColumn()
+
+    class Identifier(HybridSat):      #FHIR type: Element (http://hl7.org/fhir/element.html#1.21.0)
+        class use(HybridSat.Types):
+            usual = 'usual'
+            official = 'official'
+            temp = 'temp'
+            secondary = 'secondary (If known)'
+
+        text = CodeableConcept.text
+        system = Coding.system   #system heeft FIHR type: uri; Uniform Resource Identifier ( http://hl7.org/fhir/datatypes.html#uri)
+        version = Coding.version
+        code = Coding.code  # symbool in syntax gedefinieerd door het systeem (http://hl7.org/fhir/datatypes.html#code)
+        display = Coding.display  # weergave gedefinieerd door het systeem
+        user_selected = Coding.user_selected  # indien deze codering door de user zelf was gekozen
+
+        start = Period.start
+        period_end = Period.end
+
+    class Name(HybridSat):
+        class use(HybridSat.Types):     # zie ook utility class HumanName; #todo wat is de meest logische plaats voor deze hybridsat?
+            usual = 'usual'
+            official = 'official'
+            temp = 'temp'
+            nickname = 'nickname'
+            anonymous = 'anonymous'
+            old = 'old'
+            maiden = 'maiden'
+        text = HumanName.text
+        family = HumanName.family   # family name (or surname)
+        given = HumanName.given
+        prefix = HumanName.prefix
+        suffix = HumanName.suffix
+        start = HumanName.start
+        end = HumanName.end
+
+
+
+
+
 
 
