63e3679bf92d33b7f8c410575e247417dc3a9d3b
Costiaan Mesu <cmesu@artsenzorg.nl>
2016-10-21 18:45:50 +0200

    DWH2

--- run_vektis_agb.py
+++ run_vektis_agb.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.insert(0, '/home/costiaan/PYELT')
+sys.path.insert(0, '/home/costiaan/CLINICAL_DATAVAULT')
 
 from etl_mappings.vektis_agb.vektis_agb_proces import vektis_main()
 
