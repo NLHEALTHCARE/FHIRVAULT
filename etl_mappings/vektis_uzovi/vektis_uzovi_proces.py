71ce1731d5c8058a1f01c95a190a0dc252c9ebaa
Costiaan Mesu <cmesu@artsenzorg.nl>
2016-10-24 17:15:26 +0200

    DWH2

--- etl_mappings/vektis_uzovi/vektis_uzovi_proces.py
+++ etl_mappings/vektis_uzovi/vektis_uzovi_proces.py
@@ -1,4 +1,4 @@
-from pipelines.general_clinical_configs import general_config, vektis_uzovi_config
+from pipelines.clinics_configs import general_config, vektis_uzovi_config
 
 from domainmodels import role_domain
 from etl_mappings.vektis_uzovi.vektis_uzovi_mappings import init_sor_to_dv_mappings, init_source_to_sor_mappings
