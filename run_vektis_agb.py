7911739ad9dcd68d2e5ab0e996530bd8868c0d62
Costiaan Mesu <cmesu@artsenzorg.nl>
2016-10-24 14:30:09 +0200

    DWH2

--- run_vektis_agb.py
+++ run_vektis_agb.py
@@ -1,8 +1,9 @@
 import sys
-sys.path.insert(0, '/home/costiaan/PYELT')
-sys.path.insert(0, '/home/costiaan/CLINICAL_DATAVAULT')
 
-from etl_mappings.vektis_agb.vektis_agb_proces import vektis_main()
+sys.path.insert(0, '/home/costiaan/CLINICAL_DATAVAULT')
+sys.path.insert(0, '/home/costiaan/PYELT')
+
+from etl_mappings.vektis_agb.vektis_agb_proces import vektis_main
 
 if __name__ == '__main__':
     vektis_main()
