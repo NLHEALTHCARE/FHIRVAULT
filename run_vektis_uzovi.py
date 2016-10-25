36c0bfffdf269b1608279d4d8db1ebefa49347cf
Costiaan Mesu <cmesu@artsenzorg.nl>
2016-10-25 14:58:59 +0200

    DWH2.0 - CM

--- run_vektis_uzovi.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.insert(0, '/home/costiaan/PYELT')
-sys.path.insert(0, '/home/costiaan/CLINICAL_DATAVAULT')
-
-from etl_mappings.vektis_uzovi.vektis_uzovi_proces import run
-
-if __name__ == '__main__':
-    run()
