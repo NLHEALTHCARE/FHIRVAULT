5f77aad950f5a244009d923522e290a48a3b60c2
Costiaan Mesu <cmesu@artsenzorg.nl>
2016-10-26 18:34:35 +0200

    dwh2

--- etl_mappings/vektis_agb/vektis_agb_proces.py
+++ etl_mappings/vektis_agb/vektis_agb_proces.py
@@ -75,19 +75,12 @@ def convert_vektis_zips_to_csv(vektis_agb_config):
                         # alternatief HJ
                         data_row = []
                         start_pos = 0
-                        print(1)
                         line = line.replace(";", ":").replace("|", ":")  # dit voorkomt een error wanneer een veld een ";" bevat in de de veldwaarde
-                        print(2)
                         for field_def in import_def:
-                            print(3)
                             field_name = field_def[0]
-                            print(4)
                             field_len = field_def[1]
-                            print(5)
                             end_pos = start_pos + field_len
-                            print(6)
                             data_row.append(line[start_pos:end_pos].strip())
-                            print(7)
                             start_pos = end_pos
                         # end alternatief
                         # for i in range(len(fixed_length_indices)):
@@ -95,10 +88,9 @@ def convert_vektis_zips_to_csv(vektis_agb_config):
                         #         break
                         #     else:
                         #         data_row.append(line[fixed_length_indices[i]: fixed_length_indices[i+1]].strip())
-                        print(8)
                         data_list.append(data_row)
-                        print(9)
                     with open(path + file_name + '.csv', 'w', newline='', encoding='utf8') as fp:
+                        print(1)
                         csv_file = csv.writer(fp, delimiter=';')
                         csv_file.writerow(csv_column_names)
                         csv_file.writerows(data_list)
