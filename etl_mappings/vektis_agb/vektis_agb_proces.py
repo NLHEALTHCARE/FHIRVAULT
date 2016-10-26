dca0cf65f81829e3429619207875354da1f09a97
Costiaan Mesu <cmesu@artsenzorg.nl>
2016-10-26 18:35:11 +0200

    dwh2

--- etl_mappings/vektis_agb/vektis_agb_proces.py
+++ etl_mappings/vektis_agb/vektis_agb_proces.py
@@ -89,7 +89,7 @@ def convert_vektis_zips_to_csv(vektis_agb_config):
                         #     else:
                         #         data_row.append(line[fixed_length_indices[i]: fixed_length_indices[i+1]].strip())
                         data_list.append(data_row)
-                    with open(path + file_name + '.csv', 'w', newline='', encoding='utf8') as fp:
+                    with open(path + file_name + '.csv', 'w', newline='', encoding='latin1') as fp:
                         print(1)
                         csv_file = csv.writer(fp, delimiter=';')
                         csv_file.writerow(csv_column_names)
