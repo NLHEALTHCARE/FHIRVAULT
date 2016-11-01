cc75a769d8e145f8f78669444473a5bd1e2fb2a2
Costiaan Mesu <cmesu@artsenzorg.nl>
2016-11-01 18:23:17 +0100

    dwh2

--- etl_mappings/nhg/nhg_mappings.py
+++ etl_mappings/nhg/nhg_mappings.py
@@ -8,9 +8,8 @@ __author__ = 'cmesu'
 
 def init_source_to_sor_mappings(path):
     mappings = []
-    source_file = CsvFile(path + 'NHG-Tabel 2-Proefje.txt', delimiter="\t", encoding='utf8')
+    source_file = CsvFile(path + 'NHG-Tabel 2-Proefje.txt', delimiter='\t',  encoding='utf8')
     source_file.set_primary_key(['Code'])
-    print("CHECK DEZE", source_file.delimiter or 'leeg')
     sor_mapping = SourceToSorMapping(source_file, 'nhg2_proefje_hstage', auto_map=True)
     mappings.append(sor_mapping)
     return mappings
