8229fbcc3bc6a7c152818790747943f2687f3571
Costiaan Mesu <cmesu@artsenzorg.nl>
2016-11-07 14:59:53 +0100

    DWH2

--- domainmodel_fhir/identity_domain.py
+++ domainmodel_fhir/identity_domain.py
@@ -1,4 +1,3 @@
-from domainmodel_fhir.groups_domain import Organization
 from pyelt.datalayers.database import Columns
 from pyelt.datalayers.dv import DvEntity, Sat, Link, HybridSat, HybridLink, LinkReference
 
@@ -8,6 +7,38 @@ from pyelt.datalayers.dv import DvEntity, Sat, Link, HybridSat, HybridLink, Link
 #                                                                                                       #
 #########################################################################################################
 
+
+
+##### GROUPS #####
+
+class Organization(DvEntity):
+    class Default(Sat):
+        active = Columns.BoolColumn()
+        name = Columns.TextColumn()
+
+    class Identifier(HybridSat):
+        class Types(HybridSat.Types):
+            usual = 'usual'
+            official = 'official'
+            temp = 'temp'
+            secondary = 'secondary (If known)'
+
+        use = Columns.TextColumn(default_value=Types.official)
+        org_type = Columns.FHIR.CodeableConceptColumn()
+        system = Columns.TextColumn()
+        value = Columns.TextColumn()
+        period = Columns.FHIR.PeriodColumn()
+
+    #todo: afmaken mat andere sats
+
+class OrganizationOrganizationLink(Link):
+    organization = LinkReference(Organization)
+    linked_to_organization = LinkReference(Organization)
+
+
+
+
+##### INDIVIDUALS #####
 
 class GenderTypes:
     """enum met codes"""
--- domainmodel_fhir/test.py
+++ domainmodel_fhir/test.py
@@ -1,10 +1,8 @@
-from domainmodel_fhir import groups_domain
-from domainmodel_fhir import individuals_domain
+from domainmodel_fhir import identity_domain
 from domainmodel_fhir.test_config import test_configs
 from pyelt.pipeline import Pipeline
 
 pipeline = Pipeline(test_configs)
 pipe = pipeline.get_or_create_pipe('test2_system')
-pipe.register_domain(individuals_domain)
-pipe.register_domain(groups_domain)
+pipe.register_domain(identity_domain)
 pipeline.run()
