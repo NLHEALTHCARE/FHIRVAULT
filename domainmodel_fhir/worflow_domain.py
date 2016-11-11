431af75742551c4d12a12f5bb036d57842daaf85
Costiaan Mesu <cmesu@artsenzorg.nl>
2016-11-11 15:02:37 +0100

    DWH2

---
+++ domainmodel_fhir/worflow_domain.py
@@ -0,0 +1,10 @@
+from pyelt.datalayers.database import Columns
+from pyelt.datalayers.dv import DvEntity, Sat, Link, HybridSat, HybridLink, LinkReference
+
+#########################################################################################################
+#                                                                                                       #
+# Domein model volgens FHIR-standaard (hl7.org.fhir). Iedere Entiteit heeft als type "DomainResource"   #
+#                                                                                                       #
+#########################################################################################################
+
+##### PATIENT MANAGEMENT #####
