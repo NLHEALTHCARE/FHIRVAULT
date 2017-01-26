4876b938d2530b2278a45d9299cea737a8fa19ff
RFWiegerinck <rob.wiegerinck@nlhealthcareclinics.com>
2017-01-26 11:04:03 +0100

    proms sor to dv

--- domainmodel/questionair_domain.py
+++ domainmodel/questionair_domain.py
@@ -1,2 +1,21 @@
 from pyelt.datalayers.database import Columns
 from pyelt.datalayers.dv import *
+
+
+class Test(HubEntity):
+
+    class Default(Sat):
+        test_id = Columns.TextColumn()
+        test_label = Columns.TextColumn()
+        label_sbg = Columns.TextColumn()
+        label_sbg_total = Columns.TextColumn()
+        description = Columns.TextColumn()
+        description_large = Columns.TextColumn()
+        description_copyright = Columns.TextColumn()
+        description_report = Columns.TextColumn()
+        normgroup_id = Columns.TextColumn()
+        normgroup_label = Columns.TextColumn()
+        # source_system = Columns.TextColumn()
+        # insert_date = Columns.TextColumn
+
+
